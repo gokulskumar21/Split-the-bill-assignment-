d8c73ca7eb697787cc6afa305425005dee5d33c5
gokulskumar21 <132939596+gokulskumar21@users.noreply.github.com>
2023-10-16 18:04:04 -0400

    Added class

---
+++ SplitTheBill/SplitTheBill.cs
@@ -0,0 +1,58 @@
+﻿namespace SplitTheBill
+{
+    public class SplitTheBill
+    {
+        public static double GetSplitAmount(double amount, int count)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("The amount must be greater than zero.");
+            }
+
+            if (count <= 0)
+            {
+                throw new ArgumentException("Count of individuals to split must be greater than zero");
+            }
+            return amount / count;
+        }
+
+
+        public static Dictionary<string, double> GetTipAmount(Dictionary<string, double> amountDict, double tipPercent)
+        {
+            if (tipPercent < 0)
+            {
+                throw new ArgumentException("Tip percentage cannot be negative.");
+            }
+            Dictionary<string, double> returnDict = new();
+
+            foreach (var item in amountDict)
+            {
+                if (item.Value <= 0)
+                {
+                    throw new ArgumentException("Amount must not be zero");
+                }
+                returnDict.Add(item.Key, (item.Value * tipPercent) / 100);
+            }
+            return returnDict;
+        }
+
+        public static decimal CalculateTipPerPerson(decimal price, int numberOfPatrons, decimal tipPercent)
+        {
+            if (numberOfPatrons <= 0)
+            {
+                throw new ArgumentException("Number of patrons must be greater than zero.");
+            }
+
+            if (tipPercent < 0)
+            {
+                throw new ArgumentException("Tip percentage cannot be negative.");
+            }
+
+            decimal totalTip = price * (tipPercent / 100);
+
+            decimal tipPerPerson = totalTip / numberOfPatrons;
+
+            return tipPerPerson;
+        }
+    }
+}
